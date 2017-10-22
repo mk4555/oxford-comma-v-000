2f2c54cf8b5e0d4c71cb4a00b1b60c68bd94d247
Min Kim <mk4555@nyu.edu>
2017-10-22 13:33:13 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,6 +6,10 @@ def oxford_comma(array)
       ans += array[counter] + ", "
       counter += 1
     end
+    ans += "and " + array[array.size-1]
+  elsif array.size == 1
+    ans += array[0]
+  elsif array.size == 2
+    ans += array[0] + ", and " + array[1]
   end
-  ans += "and " + array[array.size-1]
 end
