fa6cbcdfdf2e6aa7e7901cbc29d1382d52df3609
Min Kim <mk4555@nyu.edu>
2017-10-22 13:34:22 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  ans = ""
+  counter = 0
+  if array.size > 2
+    while counter < array.size-1
+      ans += array[counter] + ", "
+      counter += 1
+    end
+    ans += "and " + array[array.size-1]
+  elsif array.size == 1
+    ans += array[0]
+  elsif array.size == 2
+    ans += array[0] + " and " + array[1]
+  end
 end
