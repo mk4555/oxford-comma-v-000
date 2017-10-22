41402f42c9969ea285dd217bc816e978d5f01b69
Min Kim <mk4555@nyu.edu>
2017-10-22 13:29:43 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-  
+  ans = ""
+  counter = 0
+  if array.size > 2
+    while counter < array.size-2
+      ans += array[counter] + ", "
+      counter += 1
+    end
+  end
 end
